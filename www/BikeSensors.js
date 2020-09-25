27f261dd013fe3eb930ff2e57c6f1d4bf23b705a
lquiroz <lui.quiroz@hotmail.com>
2020-09-25 16:30:40 -0500

    Commit

--- www/BikeSensors.js
+++ www/BikeSensors.js
@@ -1,22 +1,22 @@
 var exec = require('cordova/exec');
 
-module.exports.initBike = function (arg0, success, error) {
+// module.exports.initBike = function (arg0, success, error) {
 
-    exec(success, error, 'BikeSensors', 'initBike', [arg0]);
+//     exec(success, error, 'BikeSensors', 'initBike', [arg0]);
     
-};
+// };
 
-module.exports.startBike = function (arg0, success, error) {
+// module.exports.startBike = function (arg0, success, error) {
 
-    exec(success, error, 'BikeSensors', 'startBike', [arg0]);
+//     exec(success, error, 'BikeSensors', 'startBike', [arg0]);
 
-};
+// };
 
-module.exports.isPluginOK = function (success, error) {
+// module.exports.isPluginOK = function (success, error) {
 
-    exec(success, error, 'BikeSensors', 'isPluginOK');
+//     exec(success, error, 'BikeSensors', 'isPluginOK');
 
-};
+// };
 
 module.exports.add = function (arg0, success, error) {
 
