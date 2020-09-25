08da4a5074ddedf7bbdae4045d59009817c5e2c0
lquiroz <lui.quiroz@hotmail.com>
2020-09-25 13:23:45 -0500

    Commit public class into no static

--- src/android/BikeSensors.java
+++ src/android/BikeSensors.java
@@ -190,23 +190,23 @@ public class BikeSensors extends CordovaPlugin {
 
     public class MachineInfo {
 
-        public static  int MIN_LOAD = 1;
+        public int MIN_LOAD = 1;
     
-        public static  int MAX_LOAD = 1;
+        public int MAX_LOAD = 1;
     
-        public static  int MIN_INCLINE = 0;
+        public int MIN_INCLINE = 0;
     
-        public static  int MAX_INCLINE = 0;
+        public int MAX_INCLINE = 0;
     
-        public static  int WHEEL_DIAMETER = 0;
+        public int WHEEL_DIAMETER = 0;
     
-        public static  int CLIENT_ID = 0;
+        public int CLIENT_ID = 0;
     
-        public static  int WATT_GROUP = 0;
+        public int WATT_GROUP = 0;
     
-        public static  boolean IS_HAVE_INCLINE = false;
+        public boolean IS_HAVE_INCLINE = false;
     
-        public static  boolean IS_HAVE_FAN = false;
+        public boolean IS_HAVE_FAN = false;
     
     }
 }
